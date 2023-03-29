98c2757206adfb2b14dec7b9c4ee99ea5dd7dc70
Charlie Skibsted <charlieskibsted@gmail.com>
2023-03-29 14:55:42 +0200

    added more ugly css :)

--- src/content.jsx
+++ src/content.jsx
@@ -5,15 +5,16 @@ export const Content = () => {
   return (
     <div>
       <h3>
-        Nomen tuum "Charlie" in Latino "Carolus" sonat. Votum tuum est
-        programmare per vitam. Amas pizza et musiciam, sed odisti alia
-        cibaria. Habes cattum quem amas, et nomine eius est "Felix".
-        Habetis fratrem et sororem, sed es primogenitus. In tempore
-        liberum, legere et scribere poesis placet tibi. Hoc anno, ad
-        ludum computatralem competit, et obtinuit trophaeum. Semper ad
-        discendum paratus es, et nunquam desistas a crescere. Latine
-        discere vult, et sperat unum die linguam fluenter loqui posse.
-        Amicos multos habes, et amicitia est maxima virtus tua.
+        I am currently studying system development in .NET in Varberg
+        and have gained experience in several areas, including C#,
+        Microsoft SQL, Web development with React and Agile system
+        development in teams. I look forward to putting my knowledge
+        into practice and gaining practical experience in a real-world
+        environment. In my spare time, I work on my music project/own
+        company Laevendel where I write music and perform on live
+        stages. I also play violin in a folk music band called
+        Hallandsfolk. Other interests are: Surfing/skating and the
+        martial art Brazilian Jiu Jitsu which I hold dear.
       </h3>
     </div>
   );
